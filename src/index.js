ee086c609afbd142f774dd22de285e4ace3674ce
Anastasia Sytsevich <drozdushka@gmail.com>
2022-07-16 16:32:39 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const digitsArray = Array.from(String(Math.abs(n)));
+    return Number(digitsArray.reverse().join(''));
 }
